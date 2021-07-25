26ffd40c1b2b1328a43842ad1683b19f0d7d22b1
Richie Tauch <rtauc001@gmail.com>
2021-07-25 14:28:42 -0700

    established a server and doing the preliminary steps to establish "usercode"

---
+++ config/connection.js
@@ -0,0 +1,22 @@
+//create connecction to our database, pass Mysql info for username + pw 
+const Sequelize= require('sequelize');
+require ('dotenv').config();
+//create connection to our DB_NAME
+
+//const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+//  host: 'localhost',
+//  dialect: 'mysql',
+//  port:3306
+//});
+
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+      host: 'localhost',
+      dialect: 'mysql',
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+    });
+
+module.exports = sequelize;
