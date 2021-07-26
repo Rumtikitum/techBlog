055806b25500edab1ef14cf27cb0139e77e7133d
Richie Tauch <rtauc001@gmail.com>
2021-07-25 20:20:15 -0700

    finished login functionality but struggling to log out

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,14 @@
-//create connecction to our database, pass Mysql info for username + pw 
 const Sequelize= require('sequelize');
-require ('dotenv').config();
-//create connection to our DB_NAME
 
-//const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-//  host: 'localhost',
-//  dialect: 'mysql',
-//  port:3306
-//});
+require ('dotenv').config();
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
       dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
+      port: 3306
+
     });
 
 module.exports = sequelize;
--- models/user.js
+++ models/user.js
@@ -1,43 +1,42 @@
-// import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
-// import our database connection from config.js
+const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
-// create our User model
-class User extends Model {}
 
-// define table columns and configuration
+// create our User model
+class User extends Model {
+  // set up method to run on instance data (per user) to check password
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
+
+// create fields/columns for User model
 User.init(
   {
-    // define an id column
     id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
     },
     username: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-    // define an email column
+      type: DataTypes.STRING,
+      allowNull: false
+    },
     email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        // there cannot be any duplicate email values in this Table
-        unique: true,
-        // if allowNull is set to false, we can run data through validators before creating table
-         validate:{
-             isEmail: true
-            }
-        },
-    // define a password column
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    },
     password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-            //this means the password must be at least 4 characters
-            len: [4]
-        }
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [4]
+      }
     }
   },
   {
@@ -47,17 +46,17 @@ User.init(
         newUserData.password = await bcrypt.hash(newUserData.password, 10);
         return newUserData;
       },
-    
+
       async beforeUpdate(updatedUserData) {
         updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
         return updatedUserData;
       }
     },
-      sequelize,
-      timestamps: false,
-      freezeTableName: true,
-      underscored: true,
-      modelName: 'user'
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user'
   }
 );
 
